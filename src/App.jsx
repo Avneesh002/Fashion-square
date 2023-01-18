1ed9deb5170c8f43c21b0b41257b9cddc96c23a7
Avneesh002 <Avneeshgrover8@gmail.com>
2023-01-18 21:01:23 +0530

    basepage(homepage without login) completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Basepage from "./Pages/Basepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Basepage />
+    </div>
+  );
 }
 
 export default App;
