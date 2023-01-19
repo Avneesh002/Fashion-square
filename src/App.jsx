4a96e832746c22a0df2519082cda4aa3708e435d
Satan Sharma <satansharma7549@gmail.com>
2023-01-19 13:56:12 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Login from "./Pages/Signup_Login/Login";
 
 function App() {
   return (
     <div className="App" >
-      <h1>App</h1>
+     <Login />
     </div>
   );
 }
