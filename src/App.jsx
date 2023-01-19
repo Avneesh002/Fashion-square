0d060ddfc0e9c2fc2dab932e6a4499a87bcf5d5e
Satan Sharma <satansharma7549@gmail.com>
2023-01-19 11:18:04 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App" >
+      <h1>App</h1>
+    </div>
+  );
 }
 
 export default App;
