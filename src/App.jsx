532999250b57d5e3581d3fb7da78f4312ea951d9
RahulDudka <rahuldudka27@gmail.com>
2023-01-20 00:42:41 +0530

    sorting fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Products from "./Pages/Product Page/Products";
 // import Homepage from "./Pages/Homepage/Homepage";
 
 //import Basepage from "./Pages/Basepage";
@@ -8,7 +9,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      
+      <Products />
     </div>
   );
 }
--- src/Pages/Product Page/SingleProduct.jsx
+++ src/Pages/Product Page/SingleProduct.jsx
@@ -8,11 +8,11 @@ const SingleProduct = ({ image, price, before_disc, brand, offer_percent }) => {
         <img src={image[0]} alt="product-image" />
       </div>
       <div className={styles.priceSection}>
-        <span className={styles.price}>{price}</span>
+        <span className={styles.price}>₹ {price}</span>
         <span className={styles.before_disc}>
-          <s>{before_disc}</s>
+          <s>₹{before_disc}</s>
         </span>
-        <span className={styles.offer_percent}>{offer_percent}</span>
+        <span className={styles.offer_percent}>{offer_percent}% off</span>
       </div>
       <p className={styles.brand}>By {brand}</p>
     </div>
