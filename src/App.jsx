656cd0ec3f128308c2fa6a1705348306ad211a19
Satan Sharma <satansharma7549@gmail.com>
2023-01-20 21:35:12 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import Navbar_main from "./Components/Navbar/Navbar_main";
 // import Homepage from "./Pages/Homepage/Homepage";
 // import Products from "./Pages/Product Page/Products";
-import Navbar_main from './Components/Navbar/Navbar_main'
+import Allroutes from "./Routes/Allroutes";
 
 function App() {
   return (
     <div className="App" >
+      <Navbar_main />
+      <Allroutes />
     </div>
   );
 }
