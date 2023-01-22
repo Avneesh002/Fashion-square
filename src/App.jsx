5883e13b5d1b3dfc9b7526ea32e8fd8e9e7c22bd
Kirtiraj Ingale <kirtiraingale46@gmail.com>
2023-01-22 11:48:25 +0530

    payment page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 
 import Navbar_main from "./Components/Navbar/Navbar_main";
-// import PaymentPage from "./Pages/PaymentPage/PaymentPage";
+//import PaymentPage from "./Pages/PaymentPage/PaymentPage";
 // import AdminHome from "./Pages/AdminBlock/AdminHome";
 // import Products from "./Pages/Product Page/Products";
 // import Homepage from "./Pages/Homepage/Homepage";
 // import Products from "./Pages/Product Page/Products";
-//import Allroutes from "./Routes/Allroutes";
+import Allroutes from "./Routes/Allroutes";
 
 function App() {
   return (
