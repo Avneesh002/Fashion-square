99f362c8471f34f323fcfed33262f3eba3829d3b
sumeet.rana <sumeet.rana@innovaccer.com>
2023-02-18 13:57:48 +0530

    don't merge: code review

--- src/Redux/Product/ProductAPI.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const getProductsAPI = async () => {
-  let response = await axios.get(
-    `https://fashionsquare-database.vercel.app/products`
-  );
-  return response.data;
-};
