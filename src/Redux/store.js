93bfc4b9c507955fa5cc0ecb1d806e7a218038a9
Satan Sharma <satansharma7549@gmail.com>
2023-03-01 16:22:53 +0530

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -7,7 +7,6 @@ import {
 import thunk from "redux-thunk";
 import AdminReducer from "./AdminReducer/Admin.reducer";
 import { authReducer } from "./Auth/Auth.reducer";
-// import { cartReducer } from "./Cart/cart.Reducer";
 import { productReducer } from "./Product/Product.Reducer";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -15,7 +14,6 @@ const rootReducer = combineReducers({
   admin: AdminReducer,
   auth: authReducer,
   products: productReducer,
-  // cartItems: cartReducer,
 });
 
 export const store = legacy_createStore(
