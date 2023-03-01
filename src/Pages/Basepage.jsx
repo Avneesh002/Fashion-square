2d0c8781b54fc36888518097fe606b7fdbde7719
Satan Sharma <satansharma7549@gmail.com>
2023-03-01 16:22:18 +0530

    Update Basepage.jsx

--- src/Pages/Basepage.jsx
+++ src/Pages/Basepage.jsx
@@ -1,4 +1,4 @@
-import { Stack, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import React from "react";
 import BaseContent from "../Components/BasepageComps/BaseContent";
 
