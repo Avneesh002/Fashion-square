97605c2d7ba442954d9971a4636340449e335a27
Satan Sharma <satansharma7549@gmail.com>
2023-03-01 12:54:50 +0530

    Remove Commented Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import "./App.css";
 import Navbar_main from "./Components/Navbar/Navbar_main";
-// import PaymentPage from "./Pages/PaymentPage/PaymentPage";
-// import AdminHome from "./Pages/AdminBlock/AdminHome";
-// import Products from "./Pages/Product Page/Products";
-
-// import Homepage from "./Pages/Homepage/Homepage";
-// import Products from "./Pages/Product Page/Products";
 import { useEffect } from "react";
 import Allroutes from "./Routes/Allroutes";
 
@@ -17,7 +11,6 @@ function App() {
   return (
     <div className="App">
       <Navbar_main />
-      
       <Allroutes />
     </div>
   );
